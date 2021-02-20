9d72d5ab186ea4885eaf24431ff379e00e5009b2
Pratik <you@example.com>
2021-02-20 18:00:58 +0545

    angle

--- sketch.js
+++ sketch.js
@@ -11,6 +11,8 @@ let skeleton;
 let v1,v2;
 let first = true;
 
+
+
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
@@ -53,7 +55,7 @@ function draw() {
     }
 
     for (let i = 0; i < skeleton.length; i++) {
-      if(skeleton.length === 10 && first){
+      if(skeleton.length === 11 && first){
         console.log('skeleton', skeleton)
         first = false;
       }
@@ -67,19 +69,28 @@ function draw() {
       if(a.part === 'rightAnkle' || a.part === 'rightKnee'){
         // console.log('ankle', a)
         v1 = createVector(a.position.x-b.position.x, a.position.y-b.position.y)
+        // console.table('v1 ',a.position.x, b.position.x, a.position.y, b.position.y);
       }
 
       if(a.part === 'rightHip' || a.part === 'rightKnee'){
         // console.log('hip', a)
-        v1 = createVector(a.position.x-b.position.x, a.position.y-b.position.y)
+        v2 = createVector(a.position.x-b.position.x, a.position.y-b.position.y)
+        // console.table(a.position.x, b.position.x, a.position.y, b.position.y);
+
       }
 
       if(v1 && v2){
-        console.log('v1', v1)
-        console.log('v2', v2)
+        // console.log('v1', v1)
+        // console.log('v2', v2)
+        
         let angle = v1.angleBetween(v2);
-        print(angle);
-
+        let angleInDegree = angle * (180 / Math.PI) 
+        let time = new Date().getHours() + ':' + new Date().getMinutes()+':'+new Date().getSeconds();
+        console.table(angleInDegree,time);
+        document.querySelector('.angle').innerHTML = 'Angle in degree: '+angleInDegree
+        delete v1;
+        delete v2;
+        
       }
     }
   }
